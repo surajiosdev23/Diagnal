68a01ead4d2e3940ffe8f4e4ddb8583cab0ec192
surajiosdev23 <surajiosdev@gmail.com>
2024-02-26 22:51:12 +0530

    Initial Commit

---
+++ Diagnal/Romantic Comedy/View/RomanticComedyVC.swift
@@ -0,0 +1,17 @@
+//
+//  RomanticComedyVC.swift
+//  Diagnal
+//
+//  Created by suraj jadhav on 26/02/24.
+//
+
+import UIKit
+
+class RomanticComedyVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Diagnal/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Diagnal
+//
+//  Created by suraj jadhav on 26/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
